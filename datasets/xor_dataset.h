fed1774321d41799bb74986ce5e02292d61d454e
NTherrien-625 <ntherrien829@gmail.com>
2023-06-11 22:08:01 +0000

    Added procedural XOR dataset for testing

---
+++ datasets/xor_dataset.h
@@ -0,0 +1,10 @@
+#ifndef XOR_DATASET_H
+#define XOR_DATASET_H
+
+#include "../Matrix.h"
+
+Matrixf** get_xor_points(unsigned int num_points);
+
+float* get_xor_lables(unsigned int num_points);
+
+#endif
