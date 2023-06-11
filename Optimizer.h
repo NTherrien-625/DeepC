10996f1fcd5b98c76dfc265db540d315511d7583
NTherrien-625 <ntherrien829@gmail.com>
2023-06-11 21:51:14 +0000

    Added and optimizer struct for learning parameters

---
+++ Optimizer.h
@@ -0,0 +1,18 @@
+#include OPTIMIZER_H
+#define OPTIMIZER_H
+
+#include "Matrix.h"
+
+typedef struct Optimizer {
+    // The learning rate
+    float alpha;
+
+    // The loss function
+    float (*loss)(Matrixf*, Matrixf*);
+} Optimizer;
+
+Optimizer* malloc_Optimizer(float lr, float (*func)(Matrixf*, Matrixf*));
+
+void free_Optimizer(Optimizer* O);
+
+#endif
