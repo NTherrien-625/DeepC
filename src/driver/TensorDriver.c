514597cc114d6445c8716777f3ef9b152f7a5e9b
NTherrien-625 <ntherrien829@gmail.com>
2023-07-28 16:33:44 -0400

    Better rank 2 x rank 2 driver

--- src/driver/TensorDriver.c
+++ src/driver/TensorDriver.c
@@ -9,21 +9,23 @@ int main(int argc, char** argv) {
     free_Tensord(new_tensor);
 
     // Rank 2 x Rank 2 test
-    Tensord* left = malloc_Tensord(2, 2, 2);
-    Tensord* right = malloc_Tensord(2, 2, 2);
+    unsigned int m = 2;
+    unsigned int n = 2;
+    Tensord* left = malloc_Tensord(2, m, n);
+    Tensord* right = malloc_Tensord(2, m, n);
 
-    for (unsigned int i = 0; i < 2; ++i) {
-        for (unsigned int j = 0; j < 2; ++j) {
-            left->data[i * 2 + j] = (i + j);
-            right->data[i * 2 + j] = 2 * (i + j);
+    for (unsigned int i = 0; i < m; ++i) {
+        for (unsigned int j = 0; j < n; ++j) {
+            left->data[i * n + j] = (i + j);
+            right->data[i * n + j] = 2 * (i + j);
         }
     }
 
     Tensord* mul = mul_Tensord_r2(left, right);
 
-    for (unsigned int i = 0; i < 2; ++i) {
-        for (unsigned int j = 0; j < 2; ++j) {
-            fprintf(stdout, "%f\t", mul->data[i * 2 + j]);
+    for (unsigned int i = 0; i < m; ++i) {
+        for (unsigned int j = 0; j < n; ++j) {
+            fprintf(stdout, "%f\t", mul->data[i * n + j]);
         }
         fprintf(stdout, "\n");
     }
